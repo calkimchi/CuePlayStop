7389ea3470a1a052e52ecbe2b6845cefdca229c8
calkimchi <calkimchi@gmail.com>
2020-11-29 01:25:43 -0800

    first component made from scratch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Cueplaystop from './Cueplaystop.js';
+
 function App() {
   return (
     <div className="App">
+      <Cueplaystop/>
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
